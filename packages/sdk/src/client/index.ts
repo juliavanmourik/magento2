f046642764d58890bfe7f193028bc971a360d96f
judavanmourik@gmail.com <judavanmourik@gmail.com>
2023-12-27 20:44:28 +0100

    feat: use fetch instead of axios

--- packages/sdk/src/client/index.ts
+++ packages/sdk/src/client/index.ts
@@ -1,9 +1,11 @@
 import axios, { Axios } from 'axios';
+import fetchAdapter from '@vespaiach/axios-fetch-adapter';
 import { resolveBaseUrl } from './resolvers';
 
 const client = new Proxy(axios.create(), {
   get: (axiosInstance, property) => {
     axiosInstance.defaults.withCredentials = true;
+    axiosInstance.defaults.adapter = fetchAdapter;
     axiosInstance.defaults.baseURL = resolveBaseUrl();
 
     return axiosInstance[property as keyof Axios];
